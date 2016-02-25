e4f05ae350b06fffb946907df5a392b74800d09b
jonasdegrave <jonas_degrave123@hotmail.com>
2016-02-25 10:37:20 -0300

    1st Commit

---
+++ app/src/main/java/jonasdegrave/caronuber/Carona.java
@@ -0,0 +1,51 @@
+package jonasdegrave.caronuber;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class Carona extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_carona);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMyLocationEnabled(true);
+        // Add a marker in UFRJ and move the camera
+        // Coordenadas da UFRJ: -22.857392, -43.229721
+
+
+        LatLng ufrj = new LatLng(-22.857392, -43.229721);
+        mMap.addMarker(new MarkerOptions().position(ufrj).title("Universidade Federal do Rio de Janeiro"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(ufrj));
+        mMap.moveCamera(CameraUpdateFactory.zoomTo((float)13.0));
+    }
+
+}
